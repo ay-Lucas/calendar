2410c2092fab6ed92d7b28bdc8b4c90810ea10a2
Lucas <lucasisanderson@gmail.com>
2023-06-13 22:21:35 -0400

    Added margin around the grid, began adding tasks followed by a prompt, made slight changes to theme

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const NUM_OF_BOXES = 42;
 const calendar = document.getElementById("calendar");
 const nextMonthButton = document.getElementById("next");
 const previousMonthButton = document.getElementById("previous");
+const grid = document.querySelector(".grid");
 let month = 0;
 let date = new Date();
 loadCalendar(date);
@@ -24,22 +25,30 @@ function loadCalendar(date) {
     date.getMonth(),
     0
   ).getDate();
+  let paddedSquareCount = 0;
   for (let i = firstDay; i > 0; i--) {
     let paddedSquare = document.createElement("div");
-    paddedSquare.setAttribute("id", "padded-square");
+    paddedSquareCount++;
+    paddedSquare.setAttribute("class", "padded-square-previous");
+    paddedSquare.setAttribute("id", "pad" + paddedSquareCount.toString());
     paddedSquare.innerHTML = lastMonthDayCount - i + 1;
+
     calendar.appendChild(paddedSquare);
   }
   for (let i = 0; i < thisMonthDayCount; i++) {
     let day = document.createElement("div");
     day.innerHTML = i + 1;
+    day.setAttribute("class", "this-month");
+    day.setAttribute("id", i.toString());
     calendar.appendChild(day);
   }
 
   if (firstDay + 1 + thisMonthDayCount < NUM_OF_BOXES) {
     for (let i = 0; i < NUM_OF_BOXES - (firstDay + thisMonthDayCount); i++) {
+      paddedSquareCount++;
       let paddedSquare = document.createElement("div");
-      paddedSquare.setAttribute("id", "padded-square");
+      paddedSquare.setAttribute("class", "padded-square-next");
+      paddedSquare.setAttribute("id", "pad" + paddedSquareCount.toString());
       paddedSquare.innerHTML = i + 1;
       calendar.appendChild(paddedSquare);
     }
@@ -51,10 +60,6 @@ function loadCalendar(date) {
   h1.innerHTML = month + " " + year;
   console.log(firstDay);
   console.log(thisMonthDayCount);
-}
-
-function getMonth() {
-  return new Date();
 }
 
 nextMonthButton.addEventListener("click", function (event) {
@@ -71,3 +76,23 @@ previousMonthButton.addEventListener("click", function (event) {
   console.log(previousMonth);
   loadCalendar(previousMonth);
 });
+calendar.addEventListener("click", function (event) {
+  // if(event.)
+  let task = document.createElement("div");
+  task.setAttribute("class", "task");
+  let square = document.getElementById(event.target.id);
+  console.log(event.target.id);
+  square.append(task);
+  taskMenu(event);
+});
+
+function taskMenu(event) {
+  let menu = document.createElement("div");
+  menu.setAttribute("id", "task-menu");
+  event.clientY;
+  event.clientX;
+  grid.append(menu);
+  menu.ce;
+
+  // calendar.append(menu);
+}
